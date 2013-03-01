19fb4af0f4f728feb705c341964e5b66b6ac084b
Andres A Garcia P <codigotutor@gmail.com>
2013-03-01 11:57:07 -0500

    version inicial

---
+++ JumperTutorial/JumperTutorial/src/us/andresgarcia/GdxGameJumperTutorial.java
@@ -0,0 +1,34 @@
+package us.andresgarcia;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class GdxGameJumperTutorial implements ApplicationListener {
+	
+	
+	@Override
+	public void create() {		
+	
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void render() {		
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
